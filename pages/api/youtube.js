3532a78a4d6a872f03d2b9f4908b8fa5c6493fec
coding-to-music <connors.tom@gmail.com>
2022-05-09 06:39:51 +0000

    Build

--- pages/api/youtube.js
+++ pages/api/youtube.js
@@ -1,12 +1,15 @@
 export default async (req, res) => {
-    var key = process.env.YOUTUBE_KEY
-    const response = await fetch('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCLMdmCCRFGWt7rktx6tMErw&key=' + key)
-    const json = await response.json()
-    const { subscriberCount, viewCount, videoCount } = json.items[0].statistics
+  var key = process.env.YOUTUBE_KEY;
+  const response = await fetch(
+    "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCAw8gbRBTKZHL7QG0PZFpXA&key=" +
+      key
+  );
+  const json = await response.json();
+  const { subscriberCount, viewCount, videoCount } = json.items[0].statistics;
 
-    return res.status(200).json({
-        subscriberCount,
-        viewCount,
-        videoCount,
-    })
-}
+  return res.status(200).json({
+    subscriberCount,
+    viewCount,
+    videoCount,
+  });
+};
